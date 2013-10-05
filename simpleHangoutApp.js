8c3f6522b298c9df9bb992db3445b6d61f8c38fd
jalman <jalman@mit.edu>
2013-10-05 16:15:53 -0400

    first commit!

---
+++ simpleHangoutApp.js
@@ -0,0 +1,55 @@
+var roles = ['resistance','resistance', 'resistance','spy','spy','resistance','spy','resistance','resistance','spy']
+function shuffle(array) {
+  var currentIndex = array.length
+    , temporaryValue
+    , randomIndex
+    ;
+
+  while (0!== currentIndex){
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array
+}
+
+function showParticipants() {
+  var participants = gapi.hangout.getParticipants();
+  var retVal = '<p>Participants5: </p><ul>';
+  for (var i in participants) {
+    participant = participants[i];
+    retVal += '<p>' + participant.person.displayName + '<p>';
+  }
+  if (participants[0] === gapi.hangout.getLocalParticipant()) {
+    for (var index in participants) {
+      var participant = participants[index];
+
+      if (!participant.person) {
+        retVal += '<li>A participant not running this app</li>';
+      }
+      retVal += '<li>' + participant.person.displayName + "-" + shuffle(roles)[index] + '</li>';
+    }
+  }  
+  retVal += '</ul>';
+
+  var div = document.getElementById('participantsDiv');
+
+  div.innerHTML = retVal;
+}
+
+function init() {
+  // When API is ready...                                                         
+  gapi.hangout.onApiReady.add(
+      function(eventObj) {
+        if (eventObj.isApiReady) {
+          document.getElementById('showParticipants')
+            .style.visibility = 'visible';
+        }
+      });
+}
+
+// Wait for gadget to load.                                                       
+gadgets.util.registerOnLoadHandler(init);
